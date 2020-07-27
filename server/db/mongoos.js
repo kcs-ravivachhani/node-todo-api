73a8f21037eb23e64b8f58c1c9c598e8be54237e
Ravi Vachhani <ravi.vachhani@kcsitglobal.com>
2020-07-27 10:45:27 +0530

    setup heroku

--- server/db/mongoos.js
+++ server/db/mongoos.js
@@ -1,7 +1,8 @@
 var mongoos = require('mongoose');
 
 mongoos.Promise = global.Promise;
-mongoos.connect('mongodb://localhost:27017/TodoApp');
+//mongoos.connect('mongodb://localhost:27017/TodoApp');
+mongoos.connect('mongodb://heroku_hl1k9ggr:olkgobajbdvgfq482qtoloj7pg@ds017736.mlab.com:17736/heroku_hl1k9ggr');
 
 module.exports = {
 	mongoos
