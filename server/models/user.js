caf2fc2c54777880add2ba5f7c9bbba9fd7edd32
Ravi Vachhani <ravi.vachhani@kcsitglobal.com>
2020-07-28 10:42:50 +0530

    create middleware and hashing for private route

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,74 @@
-var mongoos = require('mongoose');
+const mongoos = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoos.model('Users', {
+var UserSchema = new mongoos.Schema({
 	email: {
 		type: String,
 		required: true,
 		trim: true,
-		minlength: 1
-	}
+		minlength: 1,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
 
-// var newUser = new User({
-// 	email: ' test@mail.com '
-// });
+UserSchema.methods.toJSON = function () {
+	var user = this;
+	var userObject = user.toObject();
 
-// newUser.save().then((user) => {
-// 	console.log(user);
-// }, (e) => {
-// 	console.log('Unable to save user', e)
-// });
+	return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123');
+
+	user.tokens.push({ access, token });
+	//user.tokens = user.tokens.concat([{ access, token }]);
+	return user.save().then(() => {
+		return token;
+	});
+};
+
+UserSchema.statics.findByToken = function (token) {
+	var User = this;
+	var decoded;
+
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	});
+};
+
+var User = mongoos.model('Users', UserSchema);
 
 module.exports = {
 	User
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { ObjectID } = require('mongodb');
 var { mangoos } = require('./db/mongoos');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = exporess();
 const port = process.env.PORT || 3000;
@@ -95,6 +96,23 @@ app.patch('/todos/:id', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send();
 	});
+});
+
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 app.listen(port, () => {
